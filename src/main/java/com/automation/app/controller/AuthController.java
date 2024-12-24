1bc860267dcf58a18183a99bf44a23ac15670342
jayaprakash802 <136257791+jayaprakash802@users.noreply.github.com>
2024-12-24 10:37:56 +0530

    bugfix

--- src/main/java/com/automation/app/controller/AuthController.java
+++ src/main/java/com/automation/app/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.automation.app.service.UserService;
 
 @RestController
 @RequestMapping("api/auth")
+@CrossOrigin("*")
 public class AuthController {
 	
 	@Autowired
