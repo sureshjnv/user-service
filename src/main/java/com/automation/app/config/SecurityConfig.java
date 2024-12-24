a8b48106dcc45ddfb676ae98b435817ca90a7a07
sureshmoida <sureshjnvvzm1@gmail.com>
2024-12-24 23:02:59 +0530

    Fixed Profile page

--- src/main/java/com/automation/app/config/SecurityConfig.java
+++ src/main/java/com/automation/app/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.automation.app.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.automation.app.service.MyUserDetailsService;
 
@@ -36,12 +41,27 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(customizer -> customizer.disable());
-		http.authorizeHttpRequests(request -> request.requestMatchers("**").permitAll().anyRequest().authenticated());
+		http.cors(Customizer.withDefaults()); // Enable CORS
+		http.authorizeHttpRequests(request -> request.requestMatchers("/api/auth/login","/api/auth/register").permitAll().anyRequest().authenticated());
 		http.httpBasic(Customizer.withDefaults());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
 		
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Frontend origin
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true); // Allow cookies or credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply CORS to all endpoints
+        return source;
+    }
+
 	
 	@Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
